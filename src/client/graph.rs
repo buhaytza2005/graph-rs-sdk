d4d73bbce187e9b7931b5d59fa1df8223c05e5d4
Mike P <buhaytza2005@gmail.com>
2024-05-28 11:08:24 +0100

    fix: add solutions back to lib & client

--- src/client/graph.rs
+++ src/client/graph.rs
@@ -61,7 +61,7 @@ use crate::reports::ReportsApiClient;
 use crate::schema_extensions::{SchemaExtensionsApiClient, SchemaExtensionsIdApiClient};
 use crate::service_principals::{ServicePrincipalsApiClient, ServicePrincipalsIdApiClient};
 use crate::sites::{SitesApiClient, SitesIdApiClient};
-//use crate::solutions::SolutionsApiClient;
+use crate::solutions::SolutionsApiClient;
 use crate::subscribed_skus::SubscribedSkusApiClient;
 use crate::subscriptions::{SubscriptionsApiClient, SubscriptionsIdApiClient};
 use crate::teams::{TeamsApiClient, TeamsIdApiClient};
@@ -471,7 +471,7 @@ impl GraphClient {
 
     api_client_impl!(sites, SitesApiClient, site, SitesIdApiClient);
 
-    //api_client_impl!(solutions, SolutionsApiClient);
+    api_client_impl!(solutions, SolutionsApiClient);
 
     api_client_impl!(
         subscribed_skus,
